e08bceac6d82d8d7305d61de8948c7c2d70e140f
Caleb Bassi <calebjbassi@gmail.com>
2018-11-29 12:24:54 -0800

    Rework events; copied from gizak/termui

--- events.go
+++ events.go
@@ -7,7 +7,7 @@ import (
 )
 
 /*
-here's the list of events which you can assign handlers too using the `On` function:
+List of events:
 	mouse events:
 		<MouseLeft> <MouseRight> <MouseMiddle>
 		<MouseWheelUp> <MouseWheelDown>
@@ -15,107 +15,55 @@ here's the list of events which you can assign handlers too using the `On` funct
 		any uppercase or lowercase letter or a set of two letters like j or jj or J or JJ
 		<C-d> etc
 		<M-d> etc
-		<up> <down> <left> <right>
-		<insert> <delete> <home> <end> <previous> <next>
-		<backspace> <tab> <enter> <escape> <space>
-		<C-<space>> etc
+		<Up> <Down> <Left> <Right>
+		<Insert> <Delete> <Home> <End> <Previous> <Next>
+		<Backspace> <Tab> <Enter> <Escape> <Space>
+		<C-<Space>> etc
 	terminal events:
-		<resize>
+		<Resize>
 */
 
-var eventStream = EventStream{
-	make(map[string]func(Event)),
-	"",
-	make(chan bool, 1),
-	make(chan tb.Event),
-}
+type EventType int
 
-type EventStream struct {
-	eventHandlers map[string]func(Event)
-	prevKey       string // previous keypress
-	stopLoop      chan bool
-	eventQueue    chan tb.Event // list of events from termbox
-}
+const (
+	KeyboardEvent EventType = iota
+	MouseEvent
+	ResizeEvent
+)
 
-// Event is a copy of termbox.Event that only contains the fields we need.
 type Event struct {
-	Key    string
+	Type    EventType
+	ID      string
+	Payload interface{}
+}
+
+// Mouse payload.
+type Mouse struct {
+	Drag bool
+	X    int
+	Y    int
+}
+
+// Resize payload.
+type Resize struct {
 	Width  int
 	Height int
-	MouseX int
-	MouseY int
 }
 
-// handleEvent calls the approriate callback function if there is one.
-func handleEvent(e tb.Event) {
-	if e.Type == tb.EventError {
-		panic(e.Err)
-	}
+var pollingChannels [](chan Event)
 
-	ne := convertTermboxEvent(e)
-
-	if val, ok := eventStream.eventHandlers[ne.Key]; ok {
-		val(ne)
-		eventStream.prevKey = ""
-	} else { // check if the last 2 keys form a key combo with a handler
-		// if this is a keyboard event and the previous event was unhandled
-		if e.Type == tb.EventKey && eventStream.prevKey != "" {
-			combo := eventStream.prevKey + ne.Key
-			if val, ok := eventStream.eventHandlers[combo]; ok {
-				ne.Key = combo
-				val(ne)
-				eventStream.prevKey = ""
-			} else {
-				eventStream.prevKey = ne.Key
-			}
-		} else {
-			eventStream.prevKey = ne.Key
-		}
-	}
-}
-
-// Loop gets events from termbox and passes them off to handleEvent.
-// Stops when StopLoop is called.
-func Loop() {
+// PollEvent gets events from termbox, converts them, then sends them to each of its channels.
+func PollEvent() <-chan Event {
+	ch := make(chan Event)
 	go func() {
-		for {
-			eventStream.eventQueue <- tb.PollEvent()
-		}
+		ch <- convertTermboxEvent(tb.PollEvent())
 	}()
-
-	for {
-		select {
-		case <-eventStream.stopLoop:
-			return
-		case e := <-eventStream.eventQueue:
-			handleEvent(e)
-		}
-	}
+	return ch
 }
 
-// StopLoop stops the event loop.
-func StopLoop() {
-	eventStream.stopLoop <- true
-}
-
-// On assigns event names to their handlers. Takes a string, strings, or a slice of strings, and a function.
-func On(things ...interface{}) {
-	function := things[len(things)-1].(func(Event))
-	for _, thing := range things {
-		if value, ok := thing.(string); ok {
-			eventStream.eventHandlers[value] = function
-		}
-		if value, ok := thing.([]string); ok {
-			for _, name := range value {
-				eventStream.eventHandlers[name] = function
-			}
-		}
-	}
-}
-
-// convertTermboxKeyValue converts a termbox keyboard event to a more friendly string format.
+// convertTermboxKeyboardEvent converts a termbox keyboard event to a more friendly string format.
 // Combines modifiers into the string instead of having them as additional fields in an event.
-func convertTermboxKeyValue(e tb.Event) string {
+func convertTermboxKeyboardEvent(e tb.Event) Event {
 	k := string(e.Ch)
 	pre := ""
 	mod := ""
@@ -127,7 +75,7 @@ func convertTermboxKeyValue(e tb.Event) string {
 		if e.Key > 0xFFFF-12 {
 			k = "<f" + strconv.Itoa(0xFFFF-int(e.Key)+1) + ">"
 		} else if e.Key > 0xFFFF-25 {
-			ks := []string{"<insert>", "<delete>", "<home>", "<end>", "<previous>", "<next>", "<up>", "<down>", "<left>", "<right>"}
+			ks := []string{"<Insert>", "<Delete>", "<Home>", "<End>", "<Previous>", "<Next>", "<Up>", "<Down>", "<Left>", "<Right>"}
 			k = ks[0xFFFF-int(e.Key)-12]
 		}
 
@@ -135,14 +83,14 @@ func convertTermboxKeyValue(e tb.Event) string {
 			pre = "<C-"
 			k = string('a' - 1 + int(e.Key))
 			kmap := map[tb.Key][2]string{
-				tb.KeyCtrlSpace:     {"C-", "<space>"},
-				tb.KeyBackspace:     {"", "<backspace>"},
-				tb.KeyTab:           {"", "<tab>"},
-				tb.KeyEnter:         {"", "<enter>"},
-				tb.KeyEsc:           {"", "<escape>"},
+				tb.KeyCtrlSpace:     {"C-", "<Space>"},
+				tb.KeyBackspace:     {"", "<Backspace>"},
+				tb.KeyTab:           {"", "<Tab>"},
+				tb.KeyEnter:         {"", "<Enter>"},
+				tb.KeyEsc:           {"", "<Escape>"},
 				tb.KeyCtrlBackslash: {"C-", "\\"},
 				tb.KeyCtrlSlash:     {"C-", "/"},
-				tb.KeySpace:         {"", "<space>"},
+				tb.KeySpace:         {"", "<Space>"},
 				tb.KeyCtrl8:         {"C-", "8"},
 			}
 			if sk, ok := kmap[e.Key]; ok {
@@ -156,49 +104,68 @@ func convertTermboxKeyValue(e tb.Event) string {
 		k += ">"
 	}
 
-	return pre + mod + k
+	id := pre + mod + k
+
+	return Event{
+		Type: KeyboardEvent,
+		ID:   id,
+	}
 }
 
-func convertTermboxMouseValue(e tb.Event) string {
-	switch e.Key {
-	case tb.MouseLeft:
-		return "<MouseLeft>"
-	case tb.MouseMiddle:
-		return "<MouseMiddle>"
-	case tb.MouseRight:
-		return "<MouseRight>"
-	case tb.MouseWheelUp:
-		return "<MouseWheelUp>"
-	case tb.MouseWheelDown:
-		return "<MouseWheelDown>"
-	case tb.MouseRelease:
-		return "<MouseRelease>"
+func convertTermboxMouseEvent(e tb.Event) Event {
+	mouseButtonMap := map[tb.Key]string{
+		tb.MouseLeft:      "<MouseLeft>",
+		tb.MouseMiddle:    "<MouseMiddle>",
+		tb.MouseRight:     "<MouseRight>",
+		tb.MouseRelease:   "<MouseRelease>",
+		tb.MouseWheelUp:   "<MouseWheelUp>",
+		tb.MouseWheelDown: "<MouseWheelDown>",
 	}
-	return ""
+
+	converted, ok := mouseButtonMap[e.Key]
+	if !ok {
+		converted = "Unknown_Mouse_Button"
+	}
+
+	Drag := false
+	if e.Mod == tb.ModMotion {
+		Drag = true
+	}
+
+	return Event{
+		Type: MouseEvent,
+		ID:   converted,
+		Payload: Mouse{
+			X:    e.MouseX,
+			Y:    e.MouseY,
+			Drag: Drag,
+		},
+	}
 }
 
 // convertTermboxEvent turns a termbox event into a termui event.
 func convertTermboxEvent(e tb.Event) Event {
-	var ne Event
+	if e.Type == tb.EventError {
+		panic(e.Err)
+	}
+
+	var event Event
 
 	switch e.Type {
 	case tb.EventKey:
-		ne = Event{
-			Key: convertTermboxKeyValue(e),
-		}
+		event = convertTermboxKeyboardEvent(e)
 	case tb.EventMouse:
-		ne = Event{
-			Key:    convertTermboxMouseValue(e),
-			MouseX: e.MouseX,
-			MouseY: e.MouseY,
-		}
+		event = convertTermboxMouseEvent(e)
 	case tb.EventResize:
-		ne = Event{
-			Key:    "<resize>",
-			Width:  e.Width,
-			Height: e.Height,
+		event = Event{
+			Type: ResizeEvent,
+			ID:   "<Resize>",
+			Payload: Resize{
+				Width:  e.Width,
+				Height: e.Height,
+			},
 		}
 	}
 
-	return ne
+	return event
 }
